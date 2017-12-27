44cb9be44c1385f10c087ad40961e38d4533ed9a
boekj03 <hansteboekhorst@gmail.com>
2017-12-27 10:29:36 +0100

    aanpassingen

--- databaseDumpTS.bat
+++ databaseDumpTS.bat
@@ -6,8 +6,10 @@ SET backupPath="C:\ws\temp\"
     Set Year=%%C   
 )    
 
+
 @echo DAY = %Day%
 @echo Month = %Month%
 @echo Year = %Year%  
   
+
 mysql -u root -p plantbeheer --password=HteBmysql2017 > "%backupPath%BACKUP%Day%-%Month%.sql"
